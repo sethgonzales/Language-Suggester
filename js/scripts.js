c8d50dd66eb85f6cfbcd0f63bf12fab468b464d5
Seth Gonzales <sethgonzales157@gmail.com>
2023-08-11 10:17:36 -0700

    add fifth form question to html, create hidden function

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Business Interface Logic
+
+
+// // User Interface Logic
+// function hideResultsAndError() {
+//     document.getElementById("c-sharp").setAttribute("class", "hidden");
+//     document.getElementById("js").setAttribute("class", "hidden");
+//     document.getElementById("python").setAttribute("class", "hidden");
+// }
+
